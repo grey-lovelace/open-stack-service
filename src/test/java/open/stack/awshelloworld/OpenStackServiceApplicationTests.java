6cf70c38e80b8c1815a0e23c0cc0ee23d882882b
grey <austin.grey.lovelace@gmail.com>
2020-03-05 14:12:43 -0600

    Add rest controller and tests

---
+++ src/test/java/open/stack/awshelloworld/OpenStackServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package open.stack.awshelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenStackServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
